8c702cf825582d38f254bbbe53fdc2b97b280268
maitep <maitepzuletaz@gmail.com>
2022-07-10 23:28:24 -0400

    zuleta zegarra maitep melanie 14023108

---
+++ src/App.stories.jsx
@@ -0,0 +1,7 @@
+import App from './App'
+export default {
+    title: 'zuletamaitep/page',
+    component: App,
+}
+const Template = args => <App  {...args}/>
+export const app= Template.bind({});
---
+++ src/components/home/Home.stories.jsx
@@ -0,0 +1,8 @@
+import Home from './Home'
+export default {
+    title: 'zuletamaitep/page',
+    component: Home,
+    
+}
+const Template = args => <Home  {...args}/>
+export const home= Template.bind({});
